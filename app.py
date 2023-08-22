840b94684bc4fc11437840ecaed79780aeff3b9f
Khallidah Idris <idriskhallidah@gmail.com>
2023-08-22 14:49:14 +0100

    IRIS FLOWER CLASSIFICATION

--- app.py
+++ app.py
@@ -8,29 +8,25 @@ pickle_in = open('model.pkl', 'rb')
 model = pickle.load(pickle_in)
 pickle_in.close()
 
-st.title("Titanic Survival Prediction")
+st.title("Iris Flower Classification")
 st.write("Developed by: Khallidah Idris")
-### inputing variables
 
-name = st.text_input("Name of the individual") 
-Pclass = st.number_input("Passenger Class (1=FirstClass, 2=SecondClass, 3=ThirdClass)")
-Sex = st.number_input("Sex (0=Male, 1=Female)") 
-Age = st.number_input("Age") 
-SibSp = st.number_input("Number of Siblings and Spouses") 
-Parch = st.number_input("Number of Parents and Children") 
-Fare = st.number_input("Fare") 
-Cabin = st.number_input("Cabin Number")
-Embarked_Q = st.checkbox("Embarked from Queens")
-Embarked_S = st.checkbox("Embarked from Southampton")
-Embarked_C = st.checkbox("Embarked from Cherbourg")
 
-button = st.button("Predict")
 
-### making predictions
+sepal_length = st.number_input("Sepal Length") 
+sepal_width = st.number_input("Sepal Width") 
+petal_length = st.number_input("Petal Length") 
+petal_width = st.number_input("Petal Width") 
+
+
+button = st.button("Classify")
+
+
 if button:
-    predictions = model.predict([[Pclass,Sex,Age,SibSp,Parch,Fare,Cabin,Embarked_Q,Embarked_S,Embarked_C]])
+    predictions = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
 if predictions == 0:
-    st.write("Unfortunately",name,"did not survive.")
+    st.write("The flower is Setosa.")
+elif predictions == 1:
+    st.write("The flower is Versicolor.")
 else:
-    st.write(name,"survived.")
-    
+    st.write("The flower is Virginica.")
